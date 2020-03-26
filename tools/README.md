a50c16b95b2ffa6c192192abbff7e3d02303bcf3
Chih-Peng Yang <cpyang@siraya.net>
2020-03-26 16:36:55 +0800

    modify descriptions

---
+++ tools/README.md
@@ -0,0 +1,7 @@
+# Tools
+
+## cert_for_pip.py
+Download MITM CA Certificate and add configuration to pip.ini 
+
+Assumption: python 2.x or 3.x installed via Anaconda
+To do this behind the MITM proxy, only minimal packages are used as pip is not actully useful before this step.
--- tools/cert_for_pip.py
+++ tools/cert_for_pip.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Add MITM CA Certificate to pip.ini 
+Download MITM CA Certificate and add configuration to pip.ini 
 
 Assumption: python 2.x or 3.x installed via Anaconda
 To do this behind the MITM proxy, only minimal packages are used as pip is not actully useful before this step.
